fe4fb5e4be0d8c9584f1caac8cf69e9d81087b9a
Vedant Chainani <vedantchainani1084@gmail.com>
2025-03-22 04:57:27 +0530

    chore: remove scrips temp and add ecdsa verify helper function

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,22 +1,31 @@
 use alloy_sol_types::sol;
 
+use ecdsa::signature::Verifier;
+use p256::ecdsa::{Signature, VerifyingKey};
+
 sol! {
     /// The public values encoded as a struct that can be easily deserialized inside Solidity.
     struct PublicValuesStruct {
-        uint32 n;
-        uint32 a;
-        uint32 b;
+        bool is_valid;
     }
 }
 
-/// Compute the n'th fibonacci number (wrapping around on overflows), using normal Rust code.
-pub fn fibonacci(n: u32) -> (u32, u32) {
-    let mut a = 0u32;
-    let mut b = 1u32;
-    for _ in 0..n {
-        let c = a.wrapping_add(b);
-        a = b;
-        b = c;
-    }
-    (a, b)
+pub fn verify_ecdsa_signature(public_key: Vec<u8>, message: Vec<u8>, signature: Vec<u8>) -> bool {
+    let sec1_pubkey: Vec<u8> = if public_key.len() == 64 {
+        let mut buf = Vec::with_capacity(65);
+        buf.push(0x04);
+        buf.extend_from_slice(public_key.as_slice());
+        buf
+    } else {
+        public_key.to_vec()
+    };
+
+    let verifying_key = VerifyingKey::from_sec1_bytes(sec1_pubkey.as_slice()).unwrap();
+    let sig = Signature::try_from(signature.as_slice()).unwrap();
+
+    // try catch verify
+
+    let is_valid = verifying_key.verify(message.as_ref(), &sig).is_ok();
+
+    is_valid
 }
--- program/src/main.rs
+++ program/src/main.rs
@@ -1,30 +1,19 @@
-//! A simple program that takes a number `n` as input, and writes the `n-1`th and `n`th fibonacci
-//! number as an output.
-
-// These two lines are necessary for the program to properly compile.
-//
-// Under the hood, we wrap your main function with some extra code so that it behaves properly
-// inside the zkVM.
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
 use alloy_sol_types::SolType;
-use fibonacci_lib::{fibonacci, PublicValuesStruct};
+use zkdnssec_lib::{verify_ecdsa_signature, PublicValuesStruct};
 
 pub fn main() {
-    // Read an input to the program.
-    //
-    // Behind the scenes, this compiles down to a custom system call which handles reading inputs
-    // from the prover.
-    let n = sp1_zkvm::io::read::<u32>();
+    let public_key = sp1_zkvm::io::read_vec();
+    let signature = sp1_zkvm::io::read_vec();
+    let message = sp1_zkvm::io::read_vec();
 
-    // Compute the n'th fibonacci number using a function from the workspace lib crate.
-    let (a, b) = fibonacci(n);
+    // Verify the signature.
+    let is_valid = verify_ecdsa_signature(public_key, signature, message);
 
     // Encode the public values of the program.
-    let bytes = PublicValuesStruct::abi_encode(&PublicValuesStruct { n, a, b });
+    let bytes = PublicValuesStruct::abi_encode(&PublicValuesStruct { is_valid });
 
-    // Commit to the public values of the program. The final proof will have a commitment to all the
-    // bytes that were committed to.
     sp1_zkvm::io::commit_slice(&bytes);
 }
